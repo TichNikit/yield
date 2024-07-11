1ff335ff71f35d42f48460b704d50fe1c07e4d01
TichNikit <168722731+TichNikit@users.noreply.github.com>
2024-07-11 10:33:50 +0300

    Add files via upload

---
+++ yield2.py
@@ -0,0 +1,28 @@
+from itertools import combinations
+
+def all_variants(text):
+    lis = []
+    for i in range(1, len(text)+1):
+        lis = []
+        b = combinations(text, i)
+        lis.extend(b)
+        for j in lis:
+            j = list(j)
+            yield (''.join(j))
+
+
+a = all_variants("abc")
+for i in a:
+    if i == 'ac':
+        continue
+    print(i)
+
+
+
+
+# a = combinations('ABCD', 2)
+# a = a
+#
+# for i in a:
+#     i = list(i)
+#     print(''.join(i))
